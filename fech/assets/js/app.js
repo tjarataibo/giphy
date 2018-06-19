1accadf0dce223a29b636b71af4e19516b09feef
Tamara <tjarataibo@gmail.com>
2018-06-19 13:41:10 -0400

    Arreglé js para que quede funcional

--- fech/assets/js/app.js
+++ fech/assets/js/app.js
@@ -2,7 +2,7 @@ const inputText = document.querySelector("input")
 // const containerTitle = document.getElementById("title")
 // const containerYear = document.getElementById("year")
 // const containerRuntime = document.getElementById("runtime")
-const containerImage = document.getElementById("img")
+const containerImage = document.getElementById("elementos")
 
 
 //enviar mensaje con enter sin boton
@@ -10,23 +10,27 @@ inputText.addEventListener("keypress", (event) => {
     // wich y keyCode son eventos del teclado
     let key = event.which || event.keyCode; //  una tecla o el codigo de esa tecla
     // 13 es enter
-    if (key === 13) { // 13 es valido para enter 
+    if (key === 13) { // 13 es valido para enter
         let gif = inputText.value;
         // console.log(movie)
         inputText.value = ""; // limpiar input
+        containerImage.innerHTML = "";
 
         fetch(`https://api.giphy.com/v1/gifs/search?api_key=w0pDBtDpXaNmRzDVpcVhUg4iTuTiI9S0&q=${gif}&limit=25&offset=0&rating=G&lang=en`)
             .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                renderInfo(data);
+            .then(dato => {
+                console.log(dato);
+                renderInfo(dato);
             })
     }
 })
-const renderInfo = data => {
+const renderInfo = dato => {
     // containerTitle.innerHTML = data.Title;
     // containerYear.innerHTML = data.Year;
     // containerRuntime.innerHTML = data.Runtime;
-    containerImage.innerHTML = `<img src="${gifImage}">`; // es valido
+    dato.data.forEach(element => {
+    let gifImage = element.images.downsized.url;
+    containerImage.innerHTML += `<img src="${gifImage}">`; // es valido
 
+});
 }
